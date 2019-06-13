77f925b98b41bac36ec8d945f0313d6775d8d6c7
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-06-13 10:57:29 +0530

    inquiry types

---
+++ application/controllers/information/Inquiry_types.php
@@ -0,0 +1,203 @@
+<?php
+
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Inquiry_types extends REST_Controller
+{
+
+    private $data = [];
+    private $error = [];
+    private $filter = [];
+    private $validations = [];
+    private $datetime_format;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('information/inquiry_types_model');
+        $this->load->library('form_validation');
+        $this->datetime_format = $this->settings_lib->config('config', 'default_date_time_format');
+        $this->form_validation->set_error_delimiters('', '');
+    }
+
+    private function getData($object)
+    {
+        $result = [];
+        if ($object):
+            $result = [
+                'id' => $object['id'],
+                'name' => $object['name'],                
+                'status' => $object['status'],
+                'status_text' => $object['status'] ? $this->lang->line('text_enable') : $this->lang->line('text_disable'),
+                'created_at' => date($this->datetime_format, strtotime($object['created_at'])),
+                'updated_at' => date($this->datetime_format, strtotime($object['updated_at'])),
+            ];
+        endif;
+        return $result;
+    }
+
+    public function index_post()
+    {
+        $this->data = [];
+        $this->data['data'] = [];
+        $this->data['status'] = true;
+
+        $list = $this->inquiry_types_model->getTables();
+
+        $result = [];
+        if ($list):
+            foreach ($list as $object):
+                $result[] = $this->getData($object);
+            endforeach;
+        else:
+            $this->data['status'] = false;
+        endif;
+
+        $this->data['recordsTotal'] = $this->inquiry_types_model->countAll();
+        $this->data['recordsFiltered'] = $this->inquiry_types_model->countFiltered();
+        $this->data['data'] = $result;
+        $this->data['message'] = $this->lang->line('text_loading');
+
+        $this->set_response($this->data, REST_Controller::HTTP_OK);
+    }
+
+    public function delete_get($id)
+    {
+        $this->data = [];
+        $this->data['data'] = [];
+        $this->data['status'] = true;
+
+        $object = $this->inquiry_types_model->deleteById($id);
+
+        $result = [];
+        if ($object):
+            $this->data['status'] = true;
+            $this->data['message'] = sprintf($this->lang->line('success_delete'), $this->lang->line('text_country'));
+            $result = $object;
+        else:
+            $this->data['status'] = false;
+            $this->data['error'] = sprintf($this->lang->line('error_delete'), $this->lang->line('text_country'));
+        endif;
+
+        $this->data['data'] = $result;
+
+        $this->set_response($this->data, REST_Controller::HTTP_OK);
+    }
+
+    public function deleteAll_post()
+    {
+        $this->data = [];
+        $this->data['data'] = [];
+        $this->data['status'] = true;
+
+        $list = json2arr($this->post('list'));
+
+        $result = [];
+        if ($list):
+            foreach ($list as $id):
+                $object = $this->inquiry_types_model->deleteById($id);
+            endforeach;
+            $this->data['status'] = true;
+            $this->data['message'] = sprintf($this->lang->line('success_delete'), $this->lang->line('text_country'));
+        else:
+            $this->data['status'] = false;
+            $this->data['error'] = sprintf($this->lang->line('error_delete'), $this->lang->line('text_country'));
+        endif;
+
+        $this->data['data'] = $result;
+
+        $this->set_response($this->data, REST_Controller::HTTP_OK);
+    }
+
+    public function detail_post()
+    {
+        $this->data = [];
+        $this->data['data'] = [];
+
+        $id = $this->post('id');
+
+        $object = $this->inquiry_types_model->getById($id);
+
+        $result = [];
+        if ($object):
+            $result = $this->getData($object);
+            $this->data['status'] = true;
+            $this->data['message'] = $this->lang->line('text_loading');
+        else:
+            $this->data['status'] = false;
+            $this->data['error'] = sprintf($this->lang->line('error_not_found'), $this->lang->line('text_country'));
+        endif;
+
+        $this->data['data'] = $result;
+
+        $this->set_response($this->data, REST_Controller::HTTP_OK);
+    }
+
+    public function save_post()
+    {
+        $this->validation();
+
+        $this->data = [];
+        $this->data['data'] = [];
+
+        $object = $this->inquiry_types_model->save();
+
+        $result = [];
+        if ($object):
+            $this->data['status'] = true;
+            $this->data['message'] = sprintf($this->lang->line('success_save'), $this->lang->line('text_country'));
+            $result = $object;
+        else:
+            $this->data['status'] = false;
+            $this->data['error'] = sprintf($this->lang->line('error_save'), $this->lang->line('text_country'));
+        endif;
+
+        $this->data['data'] = $result;
+
+        $this->set_response($this->data, REST_Controller::HTTP_OK);
+    }
+
+    public function validation()
+    {
+        $this->validations = array(
+            'name' => 'required',            
+        );
+        $this->_validation();
+    }
+
+    private function _validation()
+    {
+        $this->data = [];
+        foreach ($this->validations as $key => $validation):
+            $field = '';
+            if ($this->lang->line('text_' . $key)):
+                $field = $this->lang->line('text_' . $key);
+            else:
+                $field = humanize($key);
+            endif;
+            $this->form_validation->set_rules($key, $field, $validation);
+        endforeach;
+
+        if ($this->form_validation->run() == false):
+            foreach ($this->validations as $key => $validation):
+                if (form_error($key, '', '')):
+                    $this->error[] = array(
+                        'id' => $key,
+                        'text' => form_error($key, '', ''),
+                    );
+                endif;
+            endforeach;
+
+            $this->data['status'] = false;
+            $this->data['message'] = $this->lang->line('error_validation');
+            $this->data['result'] = $this->error;
+            echo json_encode($this->data);
+            exit;
+        endif;
+    }
+
+}
---
+++ application/models/information/Inquiry_types_model.php
@@ -0,0 +1,102 @@
+<?php
+
+class Inquiry_types_model extends CI_Model
+{
+
+    private $table = 'inquiry_types';
+    private $table_view = 'inquiry_types';
+    private $column_search = array('name', 'updated_at');
+    private $currectDatetime = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->currectDatetime = date('Y-m-d h:i:s');
+        $this->query_lib->table = $this->table;
+        $this->query_lib->table_view = $this->table_view;
+        $this->query_lib->column_search = $this->column_search;
+    }
+
+    private function _getTablesQuery()
+    {
+        $this->db->from($this->table_view);
+        $this->query_lib->where();
+        $this->query_lib->like();
+        $this->query_lib->getSearch();
+        $this->query_lib->getSort();
+    }
+
+    public function getTables()
+    {
+        $this->_getTablesQuery();
+        $this->query_lib->getPaginate();
+        $query = $this->db->get();
+        // print_r($this->db->last_query());
+        // exit;
+        return $query->result_array();
+    }
+
+    public function countFiltered()
+    {
+        $this->_getTablesQuery();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function countAll()
+    {
+        $this->db->from($this->table_view);
+        return $this->db->count_all_results();
+    }
+
+    public function getById($id)
+    {                
+        $this->db->from($this->table_view);
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+        return $query->row_array();        
+    }
+
+    public function deleteById($id)
+    {
+        $this->db->trans_start();
+        $this->db->where('id', $id);
+        $this->db->delete($this->table);
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === false):
+            $this->db->trans_rollback();
+            return false;
+        else:
+            $this->db->trans_commit();
+            return true;
+        endif;
+    }
+
+    public function save()
+    {
+        $this->db->trans_start();
+
+        $this->db->set('name', $this->input->post('name'));        
+        $this->db->set('status', $this->input->post('status'));
+
+        if ($this->input->post('id')):
+            $this->db->set('updated_at', $this->currectDatetime);
+            $id = $this->input->post('id');
+            $this->db->where('id', $id);
+            $this->db->update($this->table);
+        else:
+            $this->db->set('created_at', $this->currectDatetime);
+            $this->db->insert($this->table);
+            $id = $this->db->insert_id();
+        endif;
+
+        if ($this->db->trans_status() === false):
+            $this->db->trans_rollback();
+            return false;
+        else:
+            $this->db->trans_commit();
+            return true;
+        endif;
+    }
+
+}
